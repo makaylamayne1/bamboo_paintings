34bdcd98972c27bf081679d3778b5b4b66446004
makaylamayne1 <itsmakaylahaha@gmail.com>
2026-02-10 18:47:40 -0500

    Fresh start

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./**/*.liquid",
+    "./**/*.html",
+    "./**/*.js",
+    "./layout/**/*.liquid",
+    "./templates/**/*.liquid",
+    "./sections/**/*.liquid",
+    "./snippets/**/*.liquid",
+    "./assets/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
